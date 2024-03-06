798bc943cb594aca7fd3c617b5b76e40873c07e7
ShuttleWorth94 <zxc51483cxz@gmail.com>
2024-03-06 10:02:52 +0800

    Shuttle的提交說明

---
+++ 555/555/Models/Photo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _555.Models;
+
+public partial class Photo
+{
+    public int Id { get; set; }
+
+    public byte[]? ImageData { get; set; }
+}
